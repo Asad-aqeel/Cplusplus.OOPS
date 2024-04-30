d7e75be795031d96f461c09847870d8d8b2cde7e
ASAD AQEEL <144030369+Asad-aqeel@users.noreply.github.com>
2024-04-30 12:19:15 +0500

    Create Abstraction2.cpp

---
+++ Abstraction2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Abstraction
+{
+private:
+    int a, b;
+
+public:
+    void set(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    void display()
+    {
+        cout << "a = " << a <<"." << endl;
+        cout << "b = " << b <<"." << endl;
+    }
+};
+
+int main()
+{
+    Abstraction obj;
+    obj.set(50, 100);
+    obj.display();
+    return 0;
+}
